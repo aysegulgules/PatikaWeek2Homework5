59e7ac5d82d98609ba3dc8cdc279c614eadaafa6
aysegulgules <159700271+aysegulgules@users.noreply.github.com>
2024-07-21 20:03:27 +0300

    Add project files.

---
+++ PatikaWeek2Homework5/Program.cs
@@ -0,0 +1,35 @@
+﻿//bir şarkı sözünü yazdırılıyor
+void MethodMusic()
+{
+    Console.WriteLine("Şarkı sözü...: Kupkuru bir ağacın dalıyım yapayalnız.");
+}
+
+//Rastgele bir sayı üretip bu sayının 2'ye bölümünden kalanı hesaplama
+int MethodNumber()
+{
+    Random numberRandom = new Random();
+    int number = numberRandom.Next();
+    Console.Write($"Random seçilen {number} sayısının 2 ile bölümünden kalan...:");
+    return number %2;
+}
+
+//iki sayının çarpımını
+int MethodMultiplication(int number1, int number2)
+{
+    
+    return number1 * number2;
+}
+
+// İsim soyisim ekrana yazdırılıyor.
+void MethodName(string firstName,string lastName)
+{
+    Console.Write($"Hoş Geldiniz {firstName} {lastName}\n");
+}
+
+MethodMusic();
+
+Console.WriteLine(MethodNumber());
+
+Console.WriteLine($"9 ve 6 sayılarının çarpımı...: {MethodMultiplication(9, 6)}");
+
+MethodName("Beyza", "Yıldırım");
